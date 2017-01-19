f09f34e9527630e9df549bf9febbf815e2748b9a
Romain DUBOC <dubocr@gmail.com>
2017-01-19 15:11:53 +0000

    Implement generic methods

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var Accessory, Service, Characteristic, UUIDGen;
+var Accessory, Service, Characteristic, UUIDGen, Types;
 
 var request = require("request").defaults({ jar: true })
 var pollingtoevent = require('polling-to-event');
+
+/*
+	Overkiz states
+*/
+const STATE_CLOSURE = "core:ClosureState";
+const STATE_OPEN_CLOSED = "core:OpenClosedState";
+const STATE_LOCKED_UNLOCKED = "core:LockedUnlockedState";
+const STATE_PRIORITY_LOCK = "core:PriorityLockLevelState";
 
 module.exports = function(homebridge) {
     console.log("homebridge-overkiz API version: " + homebridge.version);
@@ -13,6 +21,7 @@ module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
+    Types = homebridge.hapLegacyTypes;
 
     // For platform plugin to be considered as dynamic platform plugin,
     // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
@@ -125,29 +134,19 @@ OverkizPlatform.prototype = {
         }, function(error, json) {
             var foundAccessories = [];
             for (device of json.devices) {
-                if (device.uiClass == 'Pod') continue;
+                if (device.uiClass == 'Pod') continue; // Skip box
                 accessory = new OverkizAccessory(that.log, that, device);
                 foundAccessories.push(accessory);
             }
             callback(foundAccessories);
         });
-    },
-    
-    getServices: function() {
-        var informationService = new Service.AccessoryInformation();
-
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Somfy")
-            .setCharacteristic(Characteristic.Model, "Bridge")
-            .setCharacteristic(Characteristic.SerialNumber, "0000-0000-0000");
-
-		return [informationService];
     }
 }
 
 function OverkizAccessory(log, platform, device) {
     // device info
-    this.log = log;
+    this.states = [];
+    this.log = platform.log;
     this.platform = platform;
 
     this.name = device.label;
@@ -155,51 +154,222 @@ function OverkizAccessory(log, platform, device) {
     this.device = device;
 }
 
-OverkizAccessory.prototype = {
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback(); // success
-	},
-	
-    getServices: function() {
+OverkizAccessory.prototype = {		
+	getServices: function() {
         // you can OPTIONALLY create an information service if you wish to override
         // the default values for things like serial number, model, etc.
         var informationService = new Service.AccessoryInformation();
 
         informationService
             .setCharacteristic(Characteristic.Manufacturer, "Somfy")
-            .setCharacteristic(Characteristic.Model, this.device.controllableName)
+            .setCharacteristic(Characteristic.Model, this.device.uiClass)
             .setCharacteristic(Characteristic.SerialNumber, this.device.deviceURL);
-
-		switch (this.device.widget) {
-            case "PositionableRollerShutter":
-            	this.targetPosition = 100;
-                this.service = new Service.WindowCovering(this.device.label);
-                this.service.getCharacteristic(Characteristic.CurrentPosition)
-                    .on('get', this.getCurrentPosition.bind(this));
-                this.service.getCharacteristic(Characteristic.TargetPosition)
-                    .on('set', this.setTargetPosition.bind(this))
-                    .on('get', this.getTargetPosition.bind(this));
-                this.service.getCharacteristic(Characteristic.PositionState)
-                    .on('set', this.getPositionState.bind(this));
-                break;
+    
+		switch (this.device.uiClass) {
+            case "RollerShutter":
+    			this.states[STATE_CLOSURE] = this.overkiz2HomekitState(STATE_CLOSURE, this._look_state(STATE_CLOSURE));
+    			service = new Service.WindowCovering(this.device.label);
+                service.getCharacteristic(Characteristic.CurrentPosition)
+                    .on('get', this.refreshState.bind(this, STATE_CLOSURE));
+                service.getCharacteristic(Characteristic.TargetPosition)
+                    .on('set', this.modifyState.bind(this, STATE_CLOSURE))
+                    .on('get', this.getState.bind(this, STATE_CLOSURE));
+                service.getCharacteristic(Characteristic.PositionState)
+                    .on('set', this.refreshState.bind(this, STATE_OPEN_CLOSED));
+            break;
+            case "DoorLock":
+    			this.states[STATE_LOCKED_UNLOCKED] = this.overkiz2HomekitState(STATE_LOCKED_UNLOCKED, this._look_state(STATE_LOCKED_UNLOCKED));
+    			service = new Service.LockMechanism(this.device.label);
+                service.getCharacteristic(Characteristic.LockCurrentState)
+                    .on('get', this.refreshState.bind(this, STATE_LOCKED_UNLOCKED));
+                service.getCharacteristic(Characteristic.LockTargetState)
+                    .on('set', this.modifyState.bind(this, STATE_LOCKED_UNLOCKED))
+                    .on('get', this.getState.bind(this, STATE_LOCKED_UNLOCKED));
+            break;
+            case "Gate":
+            	this.states[STATE_CLOSURE] = this.overkiz2HomekitState(STATE_CLOSURE, this._look_state(STATE_CLOSURE));
+    			service = new Service.Door(this.device.label);
+                service.getCharacteristic(Characteristic.CurrentPosition)
+                    .on('get', this.refreshState.bind(this, STATE_CLOSURE));
+                service.getCharacteristic(Characteristic.TargetPosition)
+                    .on('set', this.modifyState.bind(this, STATE_CLOSURE))
+                    .on('get', this.getState.bind(this, STATE_CLOSURE));
+                service.getCharacteristic(Characteristic.PositionState)
+                    .on('set', this.refreshState.bind(this, STATE_OPEN_CLOSED));
+            break;
+            case "Alarm":
+            	this.states[STATE_PRIORITY_LOCK] = this.overkiz2HomekitState(STATE_PRIORITY_LOCK, this._look_state(STATE_PRIORITY_LOCK));
+    			service = new Service.SecuritySystem(this.device.label);
+                service.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+                    .on('get', this.refreshState.bind(this, STATE_PRIORITY_LOCK));
+                service.getCharacteristic(Characteristic.SecuritySystemTargetState)
+                    .on('set', this.modifyState.bind(this, STATE_PRIORITY_LOCK))
+                    .on('get', this.getState.bind(this, STATE_PRIORITY_LOCK));
+            break;
             default:
-                break;
+            	var STATE = "OnOff";
+            	this.states[STATE] = this.overkiz2HomekitState(STATE, this._look_state(STATE));
+            	service = new Service.Switch(this.device.label);
+                service.getCharacteristic(Characteristic.On)
+                    .on('set', this.setState.bind(this, STATE))
+                    .on('get', this.getState.bind(this, STATE));
+            break;
         }
 
-		if(this.service != null)
-        	return [informationService, this.service];
-        else
-        	return [informationService];
+		return [informationService, service];
     },
+    
+    /* Generic commands */
+	
+	identify: function(callback) {
+		executeCommand('identify', [], callback, function() {});
+	},
+	
+	/*
+		Return cached state
+	*/
+	getState: function(name, callback) {
+		if(this.states[name] == undefined)
+			callback(new Error("Undefined state name " + name));
+		else
+			callback(null, this.states[name]);
+	},
+	
+	/*
+		Set state in cache
+	*/
+	setState: function(name, value, callback) {
+		this.states[name] = value;
+		callback();
+	},
+	
+    /*
+		Refresh component state
+		Return state in cache
+	*/
+	refreshState: function(name, callback) {
+		var that = this;
+        this.requestState(name, function(error, value) {
+            that.states[name] = that.overkiz2HomekitState(name, value);
+            that.log(that.name + " : " + name + "=" + that.states[name]);
+        });
+        callback(null, this.states[name]);
+	},
+	
+	/*
+		send command with immediate callback
+	*/
+	modifyState: function(name, value, callback) {
+		var that = this;
+        if(value != this.states[name]) {
+			this.states[name] = value;
+        	
+        	var params = this.homekit2OverkizCommand(name, value);
+        	var command = params.shift();;
 
+        	that.log("Set " + name + " to " + value + " => " + command + "(" + params + ")");
+        	this.executeCommand(command, params, function() {}, function() {
+			   that.requestState(name, function(error, value) {
+					that.states[name] = that.overkiz2HomekitState(name,value);
+					that.log(command + " ended, " + that.name + " : " + name + "=" + that.states[name]);
+				});
+			});
+        }
+        callback();
+	},
+	
+	/*
+		send command with real component callback
+	*/
+	modifyStateConfirm: function(name, value, callback) {
+		var that = this;
+        if(value != this.states[name]) {
+			this.states[name] = value;
+        	
+        	var cp = this.homekit2OverkizCommand(name, value);
+        	var command = cp[0];
+        	var params = cp.shift();
+        	
+        	that.log("Set " + name + " to " + value + " => " + command + "(" + params + ")");
+        	this.executeCommand(command, params, callback, function() {
+			   that.requestState(name, function(error, value) {
+					that.states[name] = that.overkiz2HomekitState(name, value);
+					that.log(command + " ended, " + that.name + " : " + name + "=" + that.states[name]);
+				});
+			});
+        }
+	},
+    
+    /*
+    	Return a converted state for Homekit
+    	'name': Overkiz name of the state to convert
+    	'value': Current value of the state
+    */
+    overkiz2HomekitState: function(name, value) {
+    	switch(name) {
+			case STATE_CLOSURE:
+				return 100-value;
+			break;
+			case STATE_LOCKED_UNLOCKED:
+				switch(value) {
+					case "locked": return Characteristic.LockCurrentState.SECURED;
+					case "unlocked": return Characteristic.LockCurrentState.UNSECURED;
+					default: return Characteristic.LockCurrentState.UNKNOWN;
+				}
+			break;
+			case STATE_OPEN_CLOSED:
+				switch(value) {
+					case "open": return Characteristic.PositionState.DECREASING; break;
+					case "close": return Characteristic.PositionState.INCREASING; break;
+					default: return Characteristic.PositionState.STOPPED; break;
+				}
+            break;
+			default:
+				return value;
+			break;
+		}
+    },
+    
+    /*
+    	Return an associated command and optional params to modify this state
+    	'name': Overkiz name of the state to update
+    	'value': Target value
+    */
+    homekit2OverkizCommand: function(name, value) {
+    	switch(name) {
+			case STATE_CLOSURE:
+				return ['setPosition', 100-value];
+			break;
+			case STATE_OPEN_CLOSED:
+				return value ? ['open'] : ['close'];
+			break;
+			case STATE_LOCKED_UNLOCKED:
+				switch(value) {
+					case Characteristic.LockCurrentState.SECURED: return ['lock'];
+					case Characteristic.LockCurrentState.UNSECURED:
+					case Characteristic.LockCurrentState.JAMMED:
+					case Characteristic.LockCurrentState.UNKNOWN: 
+					default: return ['unlock'];
+				}
+			break;
+			case STATE_PRIORITY_LOCK:
+				return value ? ['alarmOn'] : ['alarmOff'];
+			break;
+			default:
+				return ['set'+name];
+			break;
+		}
+    },
+	
+	/* Overkiz helpers */
+	
     requestState: function(state, callback) {
         var that = this;
         this.platform.get({
             url: this.platform.urlForQuery("/setup/devices/" + encodeURIComponent(this.deviceURL) + "/states/" + encodeURIComponent(state)),
             json: true
         }, function(error, json) {
-            that.log(json);
+            //that.log(json);
             callback(error, json.value);
         });
     },
@@ -214,6 +384,12 @@ OverkizAccessory.prototype = {
         });
     },
 
+	/*
+		cmdName: The command to execute
+		params: Parameter of the command
+		callback: Callback function executed when command sended
+		refresh: Callback function executed when command succeed
+	*/
     executeCommand: function(cmdName, params, callback, refresh) {
         var that = this;
         if(this.lastExecId != null) {
@@ -293,49 +469,15 @@ OverkizAccessory.prototype = {
 			}
         });
     },
-
-    getCurrentPosition(callback) {
-        var that = this;
-        this.requestState("core:ClosureState", function(error, value) {
-            var position = 100 - value;
-            that.log(that.name + " opened at " + position);
-            that.targetPosition = position;
-            //callback(error, position);
-        });
-        callback(null, this.targetPosition);
-    },
-
-	getTargetPosition(callback) {
-        callback(null, this.targetPosition);
-    },
     
-    setTargetPosition(level, callback) {
-    	//this.log("Set position to " + level);
-        var that = this;
-        if(level != this.targetPosition) {
-			this.targetPosition = level;
-        	
-        	var target = 100 - level;
-        	that.log("Set position to " + level);
-        	this.executeCommand("setPosition", [target], function() {}, function() {
-			   that.requestState("core:ClosureState", function(error, value) {
-					var position = 100 - value;
-					that.log(that.name + " opened at " + position);
-				});
-			});
-        }
-        callback();
-    },
-
-    getPositionState(callback) {
-    	this.requestState("core:OpenClosedState", function(error, value) {
-    		var positionState = Characteristic.PositionState.STOPPED;
-            switch(value) {
-            	case "open": callback(null, Characteristic.PositionState.DECREASING); break;
-            	case "close": callback(null, Characteristic.PositionState.INCREASING); break;
-            	default: callback(null, Characteristic.PositionState.STOPPED); break;
-            }
-            callback(error, position);
-        });
-    }
+    /*
+    	Look for current state
+    */
+	_look_state: function(stateName) {
+		for(state of this.device.states) {
+			if(state.name == stateName)
+				return state.value;
+		}
+		return null;
+	}
 };
