340ea5e5ac29971fe9735dc5492dff7c44e14e9a
lboue <lboue@users.noreply.github.com>
2018-07-29 13:59:26 +0200

    Update AbstractAccessory.js: Manufacturer/Model
    Expose informationService:
    Characteristic.Manufacturer
    Characteristic.Model

--- accessories/AbstractAccessory.js
+++ accessories/AbstractAccessory.js
@@ -48,6 +48,8 @@ function AbstractAccessory(log, api, device) {
 
     this.name = device.label;
     this.deviceURL = device.deviceURL;
+    this.manufacturer = "Somfy"
+    this.model = "Tahoma"
 
     this.device = device;
     
@@ -55,82 +57,91 @@ function AbstractAccessory(log, api, device) {
     
     var informationService = new Service.AccessoryInformation();
 
-		var manufacturer = this._look_state(OverkizApi.State.STATE_MANUFACTURER);
-		if (manufacturer != null)
-			informationService.setCharacteristic(Characteristic.Manufacturer, manufacturer)
-
-		var model = this._look_state(OverkizApi.State.STATE_MODEL);
-		if (model != null)
-			informationService.setCharacteristic(Characteristic.Model, model)
-
-		var parts = this.deviceURL.split("/");
-		var serial = parts[parts.length-1];
-		informationService.setCharacteristic(Characteristic.SerialNumber, serial);
+    // manufacturer
+    var manufacturer = this._look_state(OverkizApi.State.STATE_MANUFACTURER);
+    if (manufacturer != null)
+        informationService.setCharacteristic(Characteristic.Manufacturer, manufacturer)
+    else
+        informationService.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+    
+    // model
+    var model = this._look_state(OverkizApi.State.STATE_MODEL);
+    if (model != null)
+        informationService.setCharacteristic(Characteristic.Model, model)
+    else
+        informationService.setCharacteristic(Characteristic.Model, this.model)
+    
+    // parts
+    var parts = this.deviceURL.split("/");
+    
+    //serial
+    var serial = parts[parts.length-1];
+    informationService.setCharacteristic(Characteristic.SerialNumber, serial);
     this.services.push(informationService); 
 }
 
 AbstractAccessory.prototype = {
     getServices: function() {
-			return this.services;
+            return this.services;
     },
     
     onStateUpdate: function(name, value) {
         /**
-        *	Track here update of any state for this accessory
-        *	You might then update corresponding Homekit Characteristic as follow :
-        *	this.service.getCharacteristic(Characteristic.TargetPosition).updateValue(value);
+        *    Track here update of any state for this accessory
+        *    You might then update corresponding Homekit Characteristic as follow :
+        *    this.service.getCharacteristic(Characteristic.TargetPosition).updateValue(value);
         **/
     },
     
     executeCommand: function(commands, callback) {
-			var that = this;
-			var cmdName = '';
-			if(Array.isArray(commands)) {
-				cmdName = "Bulk commands";
-			} else {
-				that.log('['+that.name+'] ' + commands.name +JSON.stringify(commands.parameters));
-				cmdName = commands.name;
-				commands = [commands];
-			}
-			
-			if (this.isCommandInProgress()) {
-					this.api.cancelCommand(this.lastExecId, function() {});
-			}
-	
-			var label = cmdName + ' ' + this.name;
-			var execution = new Execution(label, this.device.deviceURL, commands);
-			
-			this.api.executeCommand(execution, function(status, error, data) {
-				if(!error) {
-					if (status == ExecutionState.INITIALIZED)
-						that.lastExecId = data.execId;
-					
-				}
-				if(status == ExecutionState.FAILED || status == ExecutionState.COMPLETED)
-					that.log('[' + that.name + '] ' + cmdName + ' ' + (error == null ? status : error));
-				else
-					that.log.debug('[' + that.name + '] ' + cmdName + ' ' + (error == null ? status : error));
-				callback(status, error, data);
-			});
+            var that = this;
+            var cmdName = '';
+            if(Array.isArray(commands)) {
+                cmdName = "Bulk commands";
+            } else {
+                that.log('['+that.name+'] ' + commands.name +JSON.stringify(commands.parameters));
+                cmdName = commands.name;
+                commands = [commands];
+            }
+            
+            if (this.isCommandInProgress()) {
+                    this.api.cancelCommand(this.lastExecId, function() {});
+            }
+    
+            var label = cmdName + ' ' + this.name;
+            var execution = new Execution(label, this.device.deviceURL, commands);
+            
+            this.api.executeCommand(execution, function(status, error, data) {
+                if(!error) {
+                    if (status == ExecutionState.INITIALIZED)
+                        that.lastExecId = data.execId;
+                    
+                }
+                if(status == ExecutionState.FAILED || status == ExecutionState.COMPLETED)
+                    that.log('[' + that.name + '] ' + cmdName + ' ' + (error == null ? status : error));
+                else
+                    that.log.debug('[' + that.name + '] ' + cmdName + ' ' + (error == null ? status : error));
+                callback(status, error, data);
+            });
     },
     
     /*
-    	Merge with another device
+        Merge with another device
     */
     merge: function(device) {
     
     },
 
     /*
-    	Look for current state
+        Look for current state
     */
     _look_state: function(stateName) {
-    	if(this.device.states != null) {
-			for (state of this.device.states) {
-				if (state.name == stateName)
-					return state.value;
-			}
-		}
+        if(this.device.states != null) {
+            for (state of this.device.states) {
+                if (state.name == stateName)
+                    return state.value;
+            }
+        }
         return null;
     },
 
@@ -139,9 +150,9 @@ AbstractAccessory.prototype = {
     },
     
     postpone: function(todo, value, callback) {
-    	var that = this;
+        var that = this;
         if(this.timeout != null) {
-        	clearTimeout(this.timeout);
+            clearTimeout(this.timeout);
         }
         this.timeout = setTimeout(function() { todo(value, function(err) { }); }, 2000);
         callback();
