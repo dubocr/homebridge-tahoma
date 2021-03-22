0a47b892fee9fec9bd65669ee88625448297a301
Romain DUBOC <romain@duboc.pro>
2021-03-22 23:22:42 +0100

    Fix stateless Awning commands

--- src/mappers/Awning.ts
+++ src/mappers/Awning.ts
@@ -1,6 +1,30 @@
 import RollerShutter from './RollerShutter';
+import { Command } from 'overkiz-client';
 
 export default class Awning extends RollerShutter {
+    protected getTargetCommands(value) {
+        if(this.stateless) {
+            if(value === 0) {
+                return new Command('close');
+            } else if(value === 100) {
+                return new Command('open');
+            } else {
+                if(this.movementDuration > 0) {
+                    const delta = value - Number(this.currentPosition!.value);
+                    const duration = Math.round(this.movementDuration * Math.abs(delta) * 1000 / 100);
+                    setTimeout(() => {
+                        this.cancelExecution();
+                    }, duration);
+                    return new Command(delta > 0 ? 'close' : 'open');
+                } else {
+                    return new Command('my');
+                }
+            }
+        } else {
+            return new Command('setClosure', this.reversedValue(value));
+        }
+    }
+
     protected reversedValue(value) {
         return this.reverse ? (100-value) : value;
     }
