32582ff240b5ae472ffb785310a58e200d2444e9
Romain DUBOC <romain@duboc.pro>
2021-07-10 12:15:36 +0200

    Manage WindowHandle

--- src/mappers/WindowHandle.ts
+++ src/mappers/WindowHandle.ts
@@ -1,5 +1,30 @@
+import { Characteristics } from '../Platform';
 import ContactSensor from './ContactSensor';
 
 export default class WindowHandle extends ContactSensor {
-    
+    protected onStateChanged(name: string, value) {
+        switch (name) {
+            case 'core:ThreeWayHandleDirectionState':
+                switch (value) {
+                    case 'closed':
+                        this.state?.updateValue(Characteristics.ContactSensorState.CONTACT_DETECTED);
+                        break;
+                    case 'tilt':
+                    case 'open':
+                        this.state?.updateValue(Characteristics.ContactSensorState.CONTACT_NOT_DETECTED);
+                        break;
+                }
+                break;
+            case 'core:OpenClosedState':
+                switch (value) {
+                    case 'closed':
+                        this.state?.updateValue(Characteristics.ContactSensorState.CONTACT_DETECTED);
+                        break;
+                    case 'open':
+                        this.state?.updateValue(Characteristics.ContactSensorState.CONTACT_NOT_DETECTED);
+                        break;
+                }
+                break;
+        }
+    }
 }
