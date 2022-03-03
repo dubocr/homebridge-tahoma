ac7ba4f61af5a9247d152a5c2e84df44d9f2051a
Romain DUBOC <romain@duboc.pro>
2022-03-03 13:01:21 +0100

    Remove state update for GarageDoor

--- src/mappers/GarageDoor.ts
+++ src/mappers/GarageDoor.ts
@@ -42,18 +42,6 @@ export default class GarageDoor extends Mapper {
             this.targetState.updateValue(Characteristics.TargetDoorState.CLOSED);
             this.currentPedestrian?.updateValue(Characteristics.LockCurrentState.SECURED);
             this.targetPedestrian?.updateValue(Characteristics.LockCurrentState.SECURED);
-        } else if (
-            this.device.states.find(
-                (s) => [
-                    'core:OpenClosedPedestrianState',
-                    'core:OpenClosedUnknownState',
-                    'core:OpenClosedState',
-                    'core:OpenClosedPartialState',
-                ].includes(s.name),
-            )
-        ) {
-
-            this.currentState.onGet(this.getCurrentState.bind(this));
         }
     }
 
