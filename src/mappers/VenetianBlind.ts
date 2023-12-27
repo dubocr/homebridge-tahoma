dubocr/homebridge-tahoma

commit b2fda5c91cc34f42b22a66b0df380196e889fc98

Update Cozytouch Authentication
Thanks @iMicknl

src/mappers/VenetianBlind.ts
@@ -42,7 +42,7 @@ export default class VenetianBlind extends RollerShutter {
     }
 
     protected getTargetCommands(value): Command | Command[] {
-        if (this.stateless) {
+        if (!this.stateless) {
             if (value === 100) {
                 return new Command('open');
             } else if (value === 0) {

commit 84c88c7ffe521c09ccd1a517db6c6aed7bae2186

Fix VenetianBlind.ts
Bug has been introduced in 
https://github.com/dubocr/homebridge-tahoma/commit/b2fda5c91cc34f42b22a66b0df380196e889fc98

causing that `blindMode` does not work, only open/close state works for VenetianBlind. Not sure what was this change supposed to do but it is breaking change.

src/mappers/VenetianBlind.ts
@@ -42,7 +42,7 @@ export default class VenetianBlind extends RollerShutter {
     }
 
     protected getTargetCommands(value): Command | Command[] {
-        if (!this.stateless) {
+        if (this.stateless) {
             if (value === 100) {
                 return new Command('open');
             } else if (value === 0) {